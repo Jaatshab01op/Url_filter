e9ec6c8535c2cdee5996b24642d9a561e7acaac7
Jaatshab01op <134989474+Jaatshab01op@users.noreply.github.com>
2023-06-03 19:50:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -55,7 +55,7 @@ class Bot(Client):
         time = curr.strftime('%I:%M:%S %p')
         app = web.AppRunner(await web_server())
         await app.setup()
-        bind_address = "0.0.0.0"
+        bind_address = "8080"
         await web.TCPSite(app, bind_address, PORT).start()
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
