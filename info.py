e974071c20fa9f18fff10de20229e192c6b95df2
Jaatshab01op <134989474+Jaatshab01op@users.noreply.github.com>
2023-06-04 09:59:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ NEWGRP = environ.get("NEWGRP", "https://graph.org/file/85b411408af4bcc5bac4c.jpg
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1987289639').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('-1001952290232').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get("CHANNELS", "-1001952290232").split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
